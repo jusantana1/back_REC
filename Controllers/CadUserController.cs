334922acb2ccd7d189a9a36ed791514ff5603f46
Julia <jumsantana12@gmail.com>
2024-09-05 18:33:07 -0300

    primeiro commit

---
+++ Controllers/CadUserController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Csharp.Controllers
+{
+    [Route("[controller]")]
+    public class CadUserController : Controller
+    {
+        private readonly ILogger<CadUserController> _logger;
+
+        public CadUserController(ILogger<CadUserController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
